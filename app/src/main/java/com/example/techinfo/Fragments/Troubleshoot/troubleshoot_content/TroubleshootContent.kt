91a8a139dbc8cd064ec8bc8b7329cd949cfde9ac
jeric <jerickogarcia0@gmail.com>
2024-09-26 11:49:57 +0800

    fixup! troubleshoot article api

---
+++ app/src/main/java/com/example/techinfo/Fragments/Troubleshoot/troubleshoot_content/TroubleshootContent.kt
@@ -0,0 +1,8 @@
+package com.example.techinfo.Fragments.Troubleshoot.troubleshootContent
+
+data class TroubleshootContent(
+    val id: String,          // Change to String to match JSON
+    val title: String,
+    val content: String      // Keep this as is
+)
+
